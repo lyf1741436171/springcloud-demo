3f2e7028e941aa8f8e092c6273f5172f3f5533f5
liaoyangfan <liaoyangfan@freedotech.com>
2019-03-22 10:58:16 +0800

    提交

---
+++ eureka-client/src/main/java/com/hudongyang/springcloud/eurekaclient1/EurekaClientApplication.java
@@ -0,0 +1,25 @@
+package com.hudongyang.springcloud.eurekaclient1;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableEurekaClient
+@SpringBootApplication
+@RestController
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    //@RequestMapping("/hi")
+    public String hi() {
+        return "访问port：" + port;
+    }
+}
